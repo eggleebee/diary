9d73031f2f0df6305b96fe0542d506d8de9ae48d
eggleebee <imimraniya@gmail.com>
2024-06-20 01:40:49 +0900

    일기 관리 기능 구현 2 작업중

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useReducer } from 'react'
+import { useReducer, useRef } from 'react'
 import { Routes, Route, Link, useNavigate } from "react-router-dom"
 import Home from './pages/Home'
 import New from './pages/New'
@@ -9,31 +9,90 @@ import Notfound from './pages/Notfound'
 
 const dummy = [
   {
-    id : 'd001',
+    id: 1,
     createDate: new Date().getTime(),
-    emotionId : 1,
-    content : "1번 일기 내용"
+    emotionId: 1,
+    content: "1번 일기 내용"
   },
   {
-    id : 'd002',
+    id: 2,
     createDate: new Date().getTime(),
-    emotionId : 2,
-    content : "2222번 일기 내용"
+    emotionId: 2,
+    content: "2222번 일기 내용"
   },
 
 ]
 
 function reducer(state, action) {
-  return state;
+  switch (action.type) {
+    case 'CREATE': return [action.data, ...state]
+    case 'UPDATE':
+      return state.map((item) =>
+        String(item.id) === String(action.data.id)
+          ? action.data
+          : item
+      )
+    case 'DELETE':
+      return state.filter((item) => String(item.id) !== String(action.id))
+    default:
+      return state;
+  }
 }
 
 const App = () => {
   /* const [data, dispatch] = useReducer(reducer, [] ) */
 
-  const [data, dispatch] = useReducer(reducer, dummy )
-  
+  const [data, dispatch] = useReducer(reducer, dummy)
+  const idRef = useRef(3)
+
+  // 새로운 일기 추가
+  const onCreate = (createDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createDate,
+        emotionId,
+        content,
+      },
+    })
+  }
+
+
+  // 기존 일기 수정
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      }
+
+    })
+  }
+
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    })
+  }
+
   return (
-    <>     
+    <>
+      <button
+        onClick={() => onCreate(new Date().getTime(), 1, "Hello")}
+      >일기추가 테스트</button>
+
+      <button
+        onClick={() => onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.")}
+      >일기수정 테스트</button>
+      <button
+        onClick={() => onDelete(1)}
+      >일기삭제 테스트</button>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
